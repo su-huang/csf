4defd9ea251b1fa34c9ff4e6b562646797bc3e7c
su-huang <suhuang2007@gmail.com>
2026-04-22 00:48:03 -0400

    add login as shared helper func

--- csf_assign05/src/client_util.cpp
+++ csf_assign05/src/client_util.cpp
@@ -12,6 +12,30 @@ Message send_recv(int fd, const Message &msg) {
   std::string response_str; 
   IO::receive(fd, response_str); 
   Wire::decode(response_str, response_msg); 
-  
+
   return response_msg; 
 }
+
+bool login(int fd, ClientMode client_mode) {
+  // get credentials 
+  std::string username, password; 
+  std::cout << "username: "; 
+  std::getline(std::cin, username); 
+  std::cout << "password: "; 
+  std::getline(std::cin, password); 
+  std::string credentials = username + '/' + password; 
+
+  // send and receive login message
+  Message login_send_msg(MessageType::LOGIN, client_mode, credentials); 
+  Message login_recv_msg = send_recv(fd, login_send_msg); 
+
+  // validate received message
+  if (login_recv_msg.get_type() == MessageType::ERROR) {
+    std::cerr << "Error: " << login_recv_msg.get_str() << "\n"; 
+    return false; 
+  } else if (login_recv_msg.get_type() != MessageType::OK) {
+    std::cerr << "Error: unknown error from login" << "\n"; 
+    return false; 
+  }
+  return true; 
+} 
