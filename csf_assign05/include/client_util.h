a6c713c05e0cc26594dab86e0efa5ef77b0e46b9
su-huang <suhuang2007@gmail.com>
2026-04-21 23:28:02 -0400

    add msg send recv helper method

--- csf_assign05/include/client_util.h
+++ csf_assign05/include/client_util.h
@@ -1,7 +1,13 @@
 #ifndef CLIENT_UTIL_H
 #define CLIENT_UTIL_H
 
+#include "message.h"
+#include "wire.h"
+#include "io.h"
+
 // TODO: classes and functions used by the client program
+// helper function to send a message and return the received message 
+Message send_recv(int fd, const Message &msg); 
 
 // This is a way that you can avoid code duplication between
 // the updater and display clients
--- csf_assign05/src/client_util.cpp
+++ csf_assign05/src/client_util.cpp
@@ -1,3 +1,17 @@
 #include "client_util.h"
 
 // TODO: definitions of common clases/functions used by client programs
+Message send_recv(int fd, const Message &msg) {
+  // encode and send msg 
+  std::string encoded_str; 
+  Wire::encode(msg, encoded_str); 
+  IO::send(encoded_str, fd); 
+
+  // receive and decode msg 
+  Message response_msg; 
+  std::string response_str; 
+  IO::receive(fd, response_str); 
+  Wire::decode(response_str, response_msg); 
+  
+  return response_msg; 
+}
